c088fc7ca20a0b3b35d3e0f7ba33e7fa8a5f3ef3
Kartik Singhal <kartiksinghal@gmail.com>
2012-08-11 11:11:13 +0530

    Updated Readme, added mind map

---
+++ Open Hack - LibShare.mm
@@ -0,0 +1,56 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1344661652987" ID="ID_887671826" MODIFIED="1344661665352" TEXT="Open Hack - LibShare" VGAP="68">
+<node CREATED="1336112665155" HGAP="17" ID="ID_1103257703" MODIFIED="1344661780660" POSITION="left" TEXT="Problem" VSHIFT="-13">
+<icon BUILTIN="help"/>
+<node CREATED="1336112733416" ID="ID_1778784254" MODIFIED="1336112777006" TEXT="Only hard copies of books are being considered."/>
+<node CREATED="1336112779380" ID="ID_574898724" MODIFIED="1336112813028" TEXT="A and B are geographically close to each other."/>
+<node CREATED="1336113460349" ID="ID_1645868430" MODIFIED="1336113466121" TEXT="Use Case Scenario">
+<node CREATED="1336112673306" ID="ID_1159973611" MODIFIED="1344661743731" TEXT="A wants to read a book, B has a copy and is ready to share with others, but A doesn&apos;t know about B or if B has a copy."/>
+</node>
+</node>
+<node CREATED="1336112818992" ID="ID_668745861" MODIFIED="1344661768215" POSITION="left" TEXT="Solution">
+<icon BUILTIN="info"/>
+<node CREATED="1336112832855" ID="ID_1415228415" MODIFIED="1336112873711" TEXT="Provide a platform to connect A with B and a database of all locally available books for borrow/sharing"/>
+</node>
+<node CREATED="1336112884935" ID="ID_525668533" MODIFIED="1336113428346" POSITION="left" TEXT="Goals">
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1336112892694" FOLDED="true" ID="ID_382562603" MODIFIED="1336113248584" TEXT="Ease of use">
+<node CREATED="1336112898555" ID="ID_685280247" MODIFIED="1336112920150" TEXT="Adding a book should be as easy as scanning a barcode."/>
+<node CREATED="1336112921132" ID="ID_1788924800" MODIFIED="1336112975532" TEXT="Identification of local communities should be as simple as entering your college name."/>
+</node>
+<node CREATED="1336112984115" FOLDED="true" ID="ID_1039960069" MODIFIED="1336113191226" TEXT="Provide detailed meta data about the books">
+<node CREATED="1336113020026" ID="ID_1350524213" MODIFIED="1336113033772" TEXT="Can be taken from sites like Goodreads"/>
+</node>
+<node CREATED="1336113038958" FOLDED="true" ID="ID_517340010" MODIFIED="1336113228826" TEXT="Awesome search">
+<node CREATED="1336113148903" ID="ID_1962104009" MODIFIED="1336113220652" TEXT="Faceted, multilingual, consider Apache Solr, Lucene etc."/>
+</node>
+</node>
+<node CREATED="1344661870824" ID="ID_891204424" MODIFIED="1344661885355" POSITION="right" TEXT="Features to focus on">
+<node CREATED="1344661886896" ID="ID_150263551" MODIFIED="1344662199541">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <ul>
+      <li>
+        Search a book using ISBN, return metadata (cover, author, desc, year, pages etc) about book
+      </li>
+      <li>
+        User registration, location/community/network
+      </li>
+      <li>
+        Personal library, marking books as available to lend, duration, lent books
+      </li>
+      <li>
+        Borrow a book - expected return date
+      </li>
+    </ul>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 LibShare
 ========
 
-Peer to peer sharing of physical books
+Peer to peer sharing of physical books.
+
+A book sharing app for local communities like a college; hard copy of books not digital.
+
+You scan a book's bar code, add it your profile, mark it as available to lend. Somebody can borrow, you can keep track of it (no loosing books any more, trying to recall who borrowed what :-)
+
+Other benefit is that some books are not easily available to order, but are sometimes just around us.
+
+Currently work in progress on a Django based web app. Plans to provide a Android app frontend as well.
