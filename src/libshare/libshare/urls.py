bd7e778d784935cd83e583d8d6a72cf04e8ef672
Souri <souriguha@gmail.com>
2012-08-11 17:55:38 +0530

    Adding index.html login

--- src/libshare/libshare/urls.py
+++ src/libshare/libshare/urls.py
@@ -15,4 +15,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('userena.urls')),
+    url(r'^$', 'libshare.views.home', name='home'),
+    
 )
