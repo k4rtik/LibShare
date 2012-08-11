f7550faf71085285bf8d7b8e9c413f76fdbedfd3
Kartik Singhal <kartiksinghal@gmail.com>
2012-08-11 16:30:08 +0530

    fixed import related error

--- src/libshare/libshare/settings.py
+++ src/libshare/libshare/settings.py
@@ -1,5 +1,5 @@
 # Django settings for libshare project.
-from os import path
+os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': path.abspath('sqlite.db'),                      # Or path to database file if using sqlite3.
+        'NAME': os.path.abspath('sqlite.db'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
