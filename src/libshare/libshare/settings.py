060f0592a6538af4abff77b168e8d52603756b8c
Kartik Singhal <kartiksinghal@gmail.com>
2012-08-11 20:48:18 +0530

    changes in templates, moved static content outside

--- src/libshare/libshare/settings.py
+++ src/libshare/libshare/settings.py
@@ -57,7 +57,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.abspath('static/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -65,6 +65,8 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.abspath('static'),
+    os.path.abspath('templates'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- src/libshare/libshare/urls.py
+++ src/libshare/libshare/urls.py
@@ -16,5 +16,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('userena.urls')),
-    url(r'^search-form/$', books.views.search_form),
+    url(r'^search/$', books.views.search_form),
 )
